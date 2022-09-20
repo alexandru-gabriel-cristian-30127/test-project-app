d44762ab7e38b1a3f3f774862e8184a0ecdec856
Ubuntu <ubuntu@ip-172-31-93-197.ec2.internal>
2022-09-20 16:45:59 +0000

    Static pages styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

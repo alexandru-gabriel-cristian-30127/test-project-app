749b85ceb344ff62e09fba02c3b5cb1debf72c3e
Ubuntu <ubuntu@ip-172-31-93-197.ec2.internal>
2022-09-26 09:40:59 +0000

    Add user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   
   def create
-    @post = current_user.post.build(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       flash[:success] = "Your data has successfully been uploaded"
       redirect_to root_url
